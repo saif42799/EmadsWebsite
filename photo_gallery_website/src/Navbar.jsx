058dad006f7c8d44b1f8412577c113165658fd41
Saif <sshaikh4@ggc.edu>
2025-09-13 18:46:53 -0400

    finished main page/ mobile verison aswell

--- photo_gallery_website/src/Navbar.jsx
+++ photo_gallery_website/src/Navbar.jsx
@@ -1,42 +1,31 @@
 
 function Navbar() {
 
-    function showSidebar(){
-        const sidebar = document.querySelector('.sidebar')
-        sidebar.style.display = 'flex'
-        sidebar.style.animation = 'slideIn 0.5s ease-out 0.5s 1 normal forwards'
-   
-    }
-
-    function hideSidebar(){
-        const sidebar = document.querySelector('.sidebar')
-        sidebar.style.display = 'none'
-        sidebar.style.animation = 'slideOut 0.5s ease-out 0.5s 1 normal forwards'
-    }
-
-
-
-
-
-    return(
+    return (
         <nav>
+
             <div className="logoDiv">
                 <img className="logoImg" src="/images/emad_Logo.png" alt="Logo"></img>
             </div>
-            
-            <ul className="sidebar">
-                <li className="x-button" onClick={() => hideSidebar()}><a><svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
-                <li id="homeMargin"><a className="navOnMoble" href="#">HOME</a></li>
-                <li><a className="navOnMoble" href="#">GALLERY</a></li>
-                <li><a className="navOnMoble" href="#">INFORMATION</a></li>
-            </ul>
 
-            <ul>
-                <li className="hideOnMoble"><a className="navHoverEffect" href="#">HOME</a></li>
-                <li className="hideOnMoble"><a className="navHoverEffect" href="#">GALLERY</a></li>
-                <li className="hideOnMoble"><a className="navHoverEffect" href="#">INFORMATION</a></li>
-                <li className="menu-button" onClick={() => showSidebar()}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
-            </ul>
+            <input type="checkbox" id="sidebar-active"></input>
+            <label for="sidebar-active" className="open-button">
+                <svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" /></svg>
+            </label>
+
+            <label id="overlay" for="sidebar-active"></label>
+
+            <div className="links-contaainer">
+                <label for="sidebar-active" className="close-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+                </label>
+
+                <ul>
+                    <li><a className="nav-Link" href="#">HOME</a></li>
+                    <li><a className="nav-Link" href="#">GALLERY</a></li>
+                    <li><a className="nav-Link" href="#">INFORMATION</a></li>
+                </ul>
+            </div>
 
             <div className="bio">
                 <div className="headDiv">

3aa23cb0597fa51b9e908a2633c313b6467b92da
Saif <sshaikh4@ggc.edu>
2025-09-12 21:59:34 -0400

    mobile view updates

--- photo_gallery_website/src/Navbar.jsx
+++ photo_gallery_website/src/Navbar.jsx
@@ -4,12 +4,19 @@ function Navbar() {
     function showSidebar(){
         const sidebar = document.querySelector('.sidebar')
         sidebar.style.display = 'flex'
+        sidebar.style.animation = 'slideIn 0.5s ease-out 0.5s 1 normal forwards'
+   
     }
 
     function hideSidebar(){
         const sidebar = document.querySelector('.sidebar')
         sidebar.style.display = 'none'
+        sidebar.style.animation = 'slideOut 0.5s ease-out 0.5s 1 normal forwards'
     }
+
+
+
+
 
     return(
         <nav>
@@ -18,17 +25,17 @@ function Navbar() {
             </div>
             
             <ul className="sidebar">
-                <li onClick={() => hideSidebar()}><a><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
-                <li><a className="navHoverEffectMoble" href="#">HOME</a></li>
-                <li><a className="navHoverEffectMoble" href="#">GALLERY</a></li>
-                <li><a className="navHoverEffectMoble" href="#">INFORMATION</a></li>
+                <li className="x-button" onClick={() => hideSidebar()}><a><svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></a></li>
+                <li id="homeMargin"><a className="navOnMoble" href="#">HOME</a></li>
+                <li><a className="navOnMoble" href="#">GALLERY</a></li>
+                <li><a className="navOnMoble" href="#">INFORMATION</a></li>
             </ul>
 
             <ul>
                 <li className="hideOnMoble"><a className="navHoverEffect" href="#">HOME</a></li>
                 <li className="hideOnMoble"><a className="navHoverEffect" href="#">GALLERY</a></li>
                 <li className="hideOnMoble"><a className="navHoverEffect" href="#">INFORMATION</a></li>
-                <li className="menu-button" onClick={() => showSidebar()}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
+                <li className="menu-button" onClick={() => showSidebar()}><a href="#"><svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 -960 960 960" width="60px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
             </ul>
 
             <div className="bio">

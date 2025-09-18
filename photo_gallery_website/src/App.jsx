d669ce127bf4048343e0e5490fe659699c8094df
Saif <sshaikh4@ggc.edu>
2025-09-17 23:35:29 -0400

    made big changes

--- photo_gallery_website/src/App.jsx
+++ photo_gallery_website/src/App.jsx
@@ -8,11 +8,53 @@ import Osaka from "./routes/collectionPages/Osaka.jsx";
 import Kyoto from "./routes/collectionPages/Kyoto.jsx";
 import NewYork from "./routes/collectionPages/NewYork.jsx";
 import Muse from "./routes/collectionPages/Muse.jsx";
+import LiquidEther from './LiquidEther';
 
 function App() {
   return (
     <>
+
+      <div style={{ width: '100%', height: '100vh', position: 'relative'  }}>
+
+        <LiquidEther
+
+          colors={['#93760B', '#B8B8B8', '#FFD500']}
+
+          mouseForce={20}
+
+          cursorSize={100}
+
+          isViscous={false}
+
+          viscous={30}
+
+          iterationsViscous={32}
+
+          iterationsPoisson={32}
+
+          resolution={0.5}
+
+          isBounce={false}
+
+          autoDemo={true}
+
+          autoSpeed={0.5}
+
+          autoIntensity={2.2}
+
+          takeoverDuration={0.25}
+
+          autoResumeDelay={3000}
+
+          autoRampDuration={0.6}
+
+        />
+
+      </div>
+
+
       <Navbar></Navbar>
+      
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/Gallery" element={<Gallery></Gallery>}></Route>
@@ -23,6 +65,8 @@ function App() {
         <Route path="/NewYork" element={<NewYork></NewYork>}></Route>
         <Route path="/Muse" element={<Muse></Muse>}></Route>
       </Routes>
+
+
     </>
   );
 }
--- photo_gallery_website/src/routes/Information.jsx
+++ photo_gallery_website/src/routes/Information.jsx
@@ -6,7 +6,57 @@ function Information() {
 
 
             <div className="info">
-                <h1>Bio</h1>
+
+                <div className="box-1">
+                    <h1>BIO:</h1>
+
+                </div>
+
+                <div className="box-2">
+                    <p>I’m Emad Shaikh, a multidisciplinary graphic designer with over three years of experience crafting visuals that blend creativity, clarity, and impact. My journey started long before university, when I taught myself Photoshop and discovered how design could transform simple ideas into compelling stories. Since then, I’ve expanded my toolkit with Illustrator, InDesign, and motion and 3D tools like After Effects, Figma, Blender, and Cinema 4D. I constantly explore new ways to bring concepts to life.</p>
+                
+                </div>
+
+                <div className="box-3">
+                    <h1>BACKGROUND:</h1>
+                </div>
+
+                <div className="box-4">
+                    <p>I’ve worked on branding, packaging, and marketing projects, most recently at SK Imports, where I developed product-focused visuals and research-driven design assets. I thrive equally in independent projects and collaborative environments, always open to feedback and inspired by different perspectives.</p>
+                </div>
+
+                <div className="box-5">
+                    <h1>DESIGN PHILOSOPHY/<br></br>STYLE:</h1>
+                </div>
+
+                <div className="box-6">
+                    <p>Living and studying in the UK before moving to New York shaped my design approach with a global perspective. I find inspiration in everything from city streets and creative fields to the smallest details that often go unnoticed.</p>
+                </div>
+
+                <div className="box-7">
+                    <h1>EXPERTISES:</h1>
+                </div>
+
+                <div className="box-8">
+                    <p>At my core, I’m a designer who loves experimenting, learning, and pushing the boundaries of what design can do to connect with people.</p>
+                </div>
+
+{/* 
+                <div className="box-9">
+                    <h1>DESIGN PHILOSOPHY/STYLE:</h1>
+                    <p>Emad, a self made artist .....@_emadshaikh</p>
+                </div>
+
+                <div className="box-10">
+                    <h1>EXPERTISES:</h1>
+                    <p>Emad, a self made artist .....@_emadshaikh</p>
+
+                </div>
+                   */}
+
+
+
+                {/* <h1>Bio</h1>
                 <p>Emad, a self made artist .....@_emadshaikh</p>
 
                 <h1>Background</h1>
@@ -16,7 +66,7 @@ function Information() {
                 <p>Emad, a self made artist .....@_emadshaikh</p>
 
                 <h1>Experites</h1>
-                <p>Emad, a self made artist .....@_emadshaikh</p>
+                <p>Emad, a self made artist .....@_emadshaikh</p> */}
 
 
             </div>

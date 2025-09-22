6b5526b5684a2fde71e3120f46658a434894e221
saifshaikh <sshaikh4@ggc.edu>
2025-09-22 12:23:26 -0400

    mac changes

--- photo_gallery_website/src/App.jsx
+++ photo_gallery_website/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
 
-<div style={{ width: '100%', height: '100vh', position: 'absolute' }}>
+<div style={{ width: '100vw', height: '100vh', position: 'fixed', top:'0', bottom:'0'}}>
   <LiquidEther
     colors={['#93760B', '#B8B8B8', '#FFD500']}
     isViscous={false}
